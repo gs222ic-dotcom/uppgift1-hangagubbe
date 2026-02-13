9fee3c2656f2cb7b9e3b6c0bf09e9d39ed4bead5
Gordana Mihailovic Svensson <gs222ic@student.lnu.se>
2026-02-13 08:59:49 +0100

    sista redigering

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ const maxAttempts = 6;
 letterButtons.forEach(button => button.disabled = true);
 startButton.disabled = false;
 
-// Initiera ett nytt spel. Visa första bilden (h0.png),sätt bildnummer till 0
+
 
 startButton.addEventListener ("click",startGame); 
 
@@ -90,19 +90,19 @@ letterButtons.forEach(button =>
 );
 
 
-// start med ny spel
+// Starta nytt spel. Visa första bilden (h0.png),sätt bildnummer till 0
 function startGame() {
 
 infoElement.innerHTML= "";
 guessNr = 0; //
-hangmanImage.src= hangmanImages[0]; // sätt bildnummer till 0 
+hangmanImage.src= hangmanImages[0]; 
  startTime = Date.now(); //för att kunna räkna tiden spelaren har från början till slut
 
 letterButtons.forEach(button => {
     button.disabled = false;
   });
  
-startButton.disabled = true; // aktiveras startknapp 
+startButton.disabled = true; // inaktiveras startknapp 
 selectRandomWord();// för att välja ett slumpmässigt ord
 
 console.log ("Spel har startats");
@@ -119,7 +119,7 @@ function selectRandomWord() {
   const boxes = document.querySelector("#boxes");
   boxes.innerHTML= ""; // radera gammalt
   boxElements = []; 
-// att spelaren välja nytt ord som är inte samma som den innan
+// loop att spelaren välja nytt ord som är inte samma som den innan 
   do {
   randomWord = wordList[Math.floor(Math.random() * wordList.length)];
 } while (randomWord === previousWord);
@@ -150,45 +150,48 @@ function guessLetter(event) {
   const letter = button.value;// väde på bokstaven
   button.disabled=true; //detta ska inakrivera knappen
   
-  //VILLKORSATSER FÖR ATT kontrollera 
-  //kontrol om bokstaven finns i ordet
-  let hittad = false;
+  
+  let hittad = false;//kontrol om bokstaven finns i ordet
+  // Loop- den gå igenom ordets bokstav för bokstav för att jämföra med gissningen
   for (let i = 0; i < randomWord.length; i++) {
+    // Villkorsats-jämföra om bokstaven på plats [i] är samma som den spelaren tryckte på
     if (randomWord[i] === letter) {
       boxElements[i].innerText = letter;
       boxElements[i].classList.add("marked");
       hittad = true;
     }
   }
-
+//Villkorsats som kontrollera om bokstaven INTE finns i ordet efter att loopen kört klart
   if (!hittad) {
     guessNr++;
     hangmanImage.src = hangmanImages[guessNr];
   }
 
-//kontroll om spelaren har förlurat
-  if (guessNr === maxAttempts) { //max gräns 6 
+//villkorsats för kontroll om spelaren har förlurat 
+  if (guessNr === maxAttempts) { 
     endGame(true);
-    return; // stopar funktionen o avslutas körning av den
+    return; 
   }
 
 
 // kontroll att alla bokstäver hittats
 let allaHittade = true;
+
 for (let i= 0; i<boxElements.length; i++) {
   if(!boxElements [i].classList.contains ("marked")){
     allaHittade = false;
+     break;
   }
 }
   if (allaHittade) { // om alla bosktäver har hittats
     endGame(false);
-  
+    
     }
   }
 // --------------------------------------------------
-// Avsluta spelet genom att skriva ut ett meddelande och
+
+
 // sedan aktivera startknappen och inaktivera bokstavsknapparna
- // / manHanged är true eller false
 function endGame (manHanged) {
   const endTime = Date.now ();
   const totalTime = Math.floor ((endTime-startTime)/ 1000);
@@ -197,7 +200,7 @@ function endGame (manHanged) {
   letterButtons.forEach(button => button.disabled = true);
   startButton.disabled = false;
 
-
+// att visa slutmeddelande 
   if (manHanged) { 
      console.log ( "Gubbe är hängd");
   }
